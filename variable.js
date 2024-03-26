7538f9bbcf294b62396b718c4916e597aa3b6101
AynurSalimli <aynur.selimli@ascca.edu.az>
2024-03-26 12:03:30 +0400

    variables

---
+++ variable.js
@@ -0,0 +1,51 @@
+//Documente yazmaq ucun 
+// document.write("Hello World")
+
+// Alert mesajini yazmaq ucun 
+// alert("Hello World")
+// console.log("Hello World");
+
+//variables
+//let const var
+
+//var 
+var name = "Aynur"
+console.log(name);
+
+//let 
+// let serverName;
+// console.log(serverName);  //undefined
+
+// serverName = "https://kodluyoruz.org"
+// console.log(serverName);
+
+// console.log(fullName);
+// let fullName = "Aynur Salimli" //error
+// var fullName = "Aynur Salimli" //undefined
+// const fullName = "Aynur Salimli" //error
+
+let lastname = "Salimli"
+lastname = "Lorem Ipsum dolor"
+console.log(lastname); // Lorem Ipsum dolor
+
+//birlesdirme
+lastname += " Yeni eklenen bilgi"
+console.log(lastname); //Lorem Ipsum dolor Yeni eklenen bilgi
+
+//_ xett ile variable
+const _salam = "aynur"
+console.log(_salam);
+
+//const ile declaration
+const serverPass= "salamsalam"
+serverPass = "saskaksa" //  TypeError
+console.log(serverPass);
+
+//camelCase case sensitive
+
+let lastName = 'Salimli'
+let kodluyoruzServerInfo = "Down"
+
+//UPPERCASE 
+let PASSWORD = "123456789"
+console.log(PASSWORD);
